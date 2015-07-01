711eeb134844aa681816df9a809b0c8c7c361379
Администратор <@WIN-9O2KB8OE3PD.mshome.net>
2015-07-01 10:04:13 +0300

    main-menu l function _ActionDB

--- js/main_menu.js
+++ js/main_menu.js
@@ -14,20 +14,42 @@
             
         });
     });
-function _save(url, out){
-    var url = url;
-    var out = out;
-alert(url+" "+out);
+    
+    function _ActionDB(){
+        
+        this.url = "";
+        this.query = "";
+        
+        this.save = function(url, query){
+            this.url = url;
+            this.query = query;
+            $.ajax({
+                asinc:false,
+                url:this.url,
+                type:'post',
+                responce:'text',
+                data:{'query':this.query},
+                success:function(){        
+                    document.location.reload();
+                } 
+            });
+        }
+    }
+    
+    function _save(url, out){
+        var url = url;
+        var out = out;
+    //alert(url+" "+out);
 
-    $.ajax({
-        asinc:false,
-        url:url,
-        type:'post',
-        responce:'text',
-        data:{'query':out},
-        success:function(data){
-alert(data);
-            document.location.reload();
-        } 
-    });
+        $.ajax({
+            asinc:false,
+            url:url,
+            type:'post',
+            responce:'text',
+            data:{'query':out},
+            success:function(data){
+    //alert(data);
+                document.location.reload();
+            } 
+        });
 }
