c67a4c3c44cf40d231fbf3d97f852a978efc9504
Администратор <@WIN-9O2KB8OE3PD.mshome.net>
2015-07-06 16:16:22 +0300

    strop calculate

--- application/views/strop_view.php
+++ application/views/strop_view.php
@@ -1,6 +1,38 @@
 <h1>Розрахунок стропильної сістеми</h1>
  
   
- <div id="strop">        
-    <canvas id="a" width="680" height="400"></canvas>
+ <div id="strop"> 
+     <table>
+         <tbody>
+             <tr>
+                 <td colspan="2">
+                     <select id="type">
+                         <option value="0" selected>Виберіть тип кровельного матеріалу</option>
+                         <option value="1">М'яка</option>
+                         <option value="2">Черепиця</option>
+                         <option value="3">Профнастил</option>
+                         <option value="4">Інше</option>
+                     </select>
+                 </td>
+             </tr>
+             <tr>
+                 <td>
+                      <canvas id="a" width="560" height="330"></canvas>
+                 </td>
+                 <td>
+                     *<small>все вимірюється в сантиметрах</small>*<br/><br/>
+                     Діаметер стінового бревна - <strong>D</strong><br/><br/>
+                     <input type="text" id="D" placeholder="діаметер" size="12"/><br/><br/>
+                     Межосьова відстань між стінами - <strong>L</strong><br/><br/>
+                     <input type="text" id="L" placeholder="міжосьове" size="12"/><br/><br/>
+                     Відстань від балок до верха - <strong>H</strong><br/><br/>
+                     <input type="text" id="H" placeholder="вишина" size="12"/><br/><br/>
+                     Відстань від стіни до краю - <strong>m</strong><br/><br/>
+                     <input type="text" id="m" placeholder="відстань" size="12"/><br/><br/>
+                     <input type="button" class="btn-save" value="Порахувати"/><br/><br/>
+                 </td>
+             </tr>
+         </tbody>
+     </table>
+   
 </div>
--- js/strop.js
+++ js/strop.js
@@ -1,28 +1,72 @@
 $(document).ready(function(){
-tut();
+    
+    var huynja =  new Drows();
+    
+    $("input#D").focus();
+    
+    $("input#D").keypress(function(e){
+        if(e.which === 13){
+            $("input#L").focus();
+        }
+    });
+
+    $("input#L").keypress(function(e){
+        if(e.which === 13){
+            $("input#H").focus();
+        }
+    });
+    $("input#H").keypress(function(e){
+        if(e.which === 13){
+            $("input#m").focus();
+        }
+    });
+    $("input#m").keypress(function(e){
+        if(e.which === 13){
+            
+            huynja.calculate($("#D").val(),$("#L").val(),$("#H").val(),$("#m").val());
+        }
+    });
+    $("input.btn-save").mousedown(function(){
+//        alert(huynja.calculate($("#D").val(),$("#L").val(),$("#H").val(),$("#m").val()));
+        huynja.calculate($("#D").val(),$("#L").val(),$("#H").val(),$("#m").val());
+    });
+    
+    
+    
 });
 
 
 
-function tut(){
+ Drows = function (){
     
     var obj = {};
     obj = document.getElementById("a");
     var context = obj.getContext("2d");
+    this.gamma = 0;
+    this.long = 0;
+    this.calculate = function (dd,ll,hh,mm){
+        var d = parseFloat(dd);
+        var l = parseFloat(ll);
+        var h = parseFloat(hh);
+        var m = parseFloat(mm);
+        var alpha = Math.ceil((Math.atan(2*h/l))*100)/100;
+        this.gamma = Math.ceil((180*alpha/Math.PI)*100)/100;
+        var ab = Math.ceil(Math.pow((Math.pow(h,2)+Math.pow((.5*l),2)),.5)*1000)/1000;
+        var ac = Math.ceil(((m+.5*d)/Math.cos(alpha))*1000)/1000;
+        this.long = ab+ac;
+        alert(this.long+" - "+this.gamma);
+    };
     
-    var wood = new Image();
-    wood.src = "../images/wood.png";
-    wood.onload = function() {
-        for(var y = 370;y > 280;y-=30){
-            context.drawImage(wood, 150,y,30,30);
-        }
-        for(var y = 370;y > 280;y-=30){
-            context.drawImage(wood, 530,y,30,30);
-        }
+    var hous = new Image();
+    hous.src = "../images/hous.png";
+    hous.onload = function() {
         
-        };
+        context.drawImage(hous, 0,0,560,330);
         
-//    context.fillRect(50, 25, 150, 100);
+    };
+    
+    context.beginPath();
+    
     for (var x = 0.5; x < 680; x += 10) {
         context.moveTo(x, 0);
         context.lineTo(x, 400);
@@ -33,63 +77,6 @@ function tut(){
         }
     context.strokeStyle = "#eee";
     context.stroke();
-    
-    context.beginPath();
-    context.moveTo(0, 40);
-    context.lineTo(330, 40);
-    context.moveTo(360, 40);
-    context.lineTo(680, 40);
-    context.moveTo(675, 35);
-    context.lineTo(680, 40);
-    context.lineTo(675, 45);
-    
-    context.moveTo(50, 0);
-    context.lineTo(50, 185);
-    context.moveTo(50, 215);
-    context.lineTo(50, 400);
-    context.moveTo(55, 395);
-    context.lineTo(50, 400);
-    context.lineTo(45, 395);
-    
-    context.moveTo(165,310);
-    context.lineTo(545, 310);
-    context.moveTo(545,310);
-    context.lineTo(545, 290);
-    context.moveTo(545,290);
-    context.lineTo(165, 290);
-    context.moveTo(165,290);
-    context.lineTo(165, 310);
-    
-    context.strokeStyle = "#000";
-    context.stroke();
 
-    context.textBaseline = "top";
-    context.fillText("( 0 , 0 )", 8, 5);
-    
-    context.textAlign = "right";
-    context.textBaseline = "bottom";
-    context.fillText("( 600 , 400 )", 672, 395);
-    
-//    context.font = "bold 12px sans-serif";
-//    context.fillText("x", 350, 43);
-//    context.fillText("y", 58, 165);
-
-    context.font = "bold 12px sans-serif";
-    context.fillText("x", 350, 43);
-    context.fillText("y", 53, 205);
-
-    context.fillRect(0, 0, 3, 3);
-    context.fillRect(677, 397, 3, 3);
-    
-//    wood = new Image();
-//    wood.src = "../images/wood.png";
-//    wood.onload = function() {
-//        alert("wood.src");
-////        for (var x = 0, y = 0;x < 500 && y < 375;x += 50, y += 37) {}
-//        context.drawImage(wood, 380,200);
-//        
-//        };
-
-
-}
+};
 
