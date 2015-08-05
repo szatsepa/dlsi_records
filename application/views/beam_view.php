52b1f6831d0ed15b3a7b1ea2a7725d2eda39321b
Администратор <@WIN-9O2KB8OE3PD.mshome.net>
2015-08-05 15:24:48 +0300

    start beams#rectangle

--- application/views/beam_view.php
+++ application/views/beam_view.php
@@ -1,5 +1,73 @@
 <h1>Розрахунок міцності БАЛКИ</h1>
  
+<div id="strop"> 
+     <table>
+         <tbody>
+             <tr>
+                 <td>
+                     <form>
+                         <input type="radio" id="rd1" value="1"/>&nbsp;&nbsp;Прямокутна
+                         <input type="radio" id="rd2" value="2"/>&nbsp;&nbsp;Кругла
+                     </form>
+                     <br/>
+                     <span id="circle" style="display: none">
+                         <input type="number" min="16" max="32" step="1" placeholder="діаметер см."/>
+                     </span>
+                     <span id="rectangle">
+                            <strong>По ГОСТ 24454-80</strong><br/><br/>
+                         <select id="b">
+                             <option value="0" selected>Виберіть ширшину бруса</option>
+                             <option value="5">50</option>
+                             <option value="6">60</option>
+                             <option value="7.5">75</option>
+                             <option value="10">100</option>
+                             <option value="12.5">125</option>
+                             <option value="15">150</option>
+                             <option value="17.5">175</option>
+                             <option value="20">200</option>
+                             <option value="22.5">225</option>
+                         </select>
+                         &nbsp;
+                         <select id="h">
+                             <option value="0" selected>Виберіть вишину бруса</option>
+                             <option value="7.5">75</option>
+                             <option value="10">100</option>
+                             <option value="12.5">125</option>
+                             <option value="15">150</option>
+                             <option value="17.5">175</option>
+                             <option value="22.5">225</option>
+                             <option value="25">250</option> 
+                             <option value="27.5">275</option>
+                         </select> 
+                     </span>
+                     
+                 </td>
+                 <td>
+                     Довжина між опорами - <strong>L</strong><br/>
+                     <input type="number" min="1" max="12" step="0.250"  id="L" value="1"/><br/>
+                     Навантаження Кг на м.п. <strong>Q</strong><br/>
+                     <input type="number" id="Q" min="50" max="2500" step="50" value="50"/>
+                     
+                 </td>
+                 
+             </tr>
+             <tr>
+                 <td>
+                     
+                     <img src="/images/beam.jpg" width="560" alt="beam.jpg"/>
+                      
+                 </td>
+                 <td id="res">
+                    
+                     <p style="text-align: center">
+                         <input type="button" class="btn-save" id="count" value="Порахувати"/>
+                     </p>
+                 </td>
+             </tr>
+             
+         </tbody>
+     </table>
+   </div>
   
  
 <script type="text/javascript" src="js/jquery.printElement.js"></script>
--- js/beam.js
+++ js/beam.js
@@ -1,7 +1,91 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function(){
+    
+    var circle = false;
+    
+    if($("input#rd2").attr('checked')){
+        $("span#circle").css({'display':'block'});
+        $("span#rectangle").css({'display':'none'});
+    }
+    
+    $(":radio").change(function(){
+        $(":radio").attr('checked',false);
+        $(this).attr('checked',true);
+        var val = $(this).val();
+        if(val === '1'){
+            $("span#rectangle").css({'display':'block'});
+            $("span#circle").css({'display':'none'});
+            circle = false;
+        }else{
+            $("span#circle").css({'display':'block'});
+            $("span#rectangle").css({'display':'none'});
+            circle = true;
+        }
+    }); 
+    
+    $("input#count").mousedown(function(){
+//            2. Статический расчет.  
+//            По  условию  задачи  имеем  расчетную  схему  однопролетной  шарнирно 
+//            опертой балки, загруженной равномерно распределенной нагрузкой. Для такой 
+//            расчетной  схемы  максимальный  момент  в  середине  балки  и  равен  M=ql2/8, 
+//            перерезывающая  сила  максимальна  на  опоре  и  равна  Q=ql/2,  максимальный 
+//             15 
+//            относительный прогиб в середине балки и равен f/l = (5qнl3)/(384EJ). Численные 
+//            значения этих величин равны: M=ql2/8  кг*м => кг*см,  
+//            Q=ql/2 , 
+
+            var M = 0,Q=0;
+            
+            var q = parseFloat($("input#Q").val()),l = parseFloat($("input#L").val()),b=parseFloat($("select#b option:selected").val()),h=parseFloat($("select#h option:selected").val());
+            ;
+            
+            
+            M = Math.ceil((q*Math.pow(l,2)/8)*10000)/10000;
+            
+            var Msm = Math.ceil(M*10000)/100;
+            
+            Q = Math.ceil(10000*q*l/2)/10000;
+            
+//            По  таблице 3  определяем  расчетное  сопротивление  растяжению  сосны, 
+//            ели,  лиственницы 2-го  сорта (п. 2а)  Rск=1,6мПа (16 кг/см2)  и  расчетное 
+//            сопротивление изгибу (п. 1а) Rи=13 мПа (130 кг/см2).  Rи=13*0,85*0,8 
+            
+            var Ri = 13*.85*.8, Rs = 1.6*.85*.8;
+            
+//            6. Вычисление геометрических характеристик для проверяемого сечения элемента. Необходимые геометрические характеристики в данном случае это: Wрасч=bh2/6= см3, Jбр=bh3/12 см3, S=bh2/8 см3 . 
+            
+            var Wr = b*Math.pow(h,2)/6, Jb =b*Math.pow(h,3),S=b*Math.pow(h,2)/8;
+            
+//            7. Проверки двух предельных состояний изгибаемого элемента. Проверяем  нормальные  напряжения  по  формуле  sи=M/Wрасч≤Rи, sи=7910,16/125=63,28 (кг/см2) < 88,4 (кг/см2) = Rи . Проверяем  касательные  напряжения  по  формуле  tск=(QS)/(Jбрbрасч)≤Rск, tск=(140,625*93,75)/(625*7,5)=2,81 (кг/см2) < 10,88 (кг/см2) = Rск . Проверяем жесткость (второе предельное состояние) по формуле  (f/l) ≤ [f/l] f/l = (5qнl3)/(384EJ) = (5*125*2,253*104)/(384*100000*625) = (71191406,25/24000000000) = 1/337. f/l = 1/337 < 1/200  = [f/l]. Здесь [f/l] = 1/200 (для прогонов и стропильных ног покрытия, таблица 16 п. 3а). 
+            var si = M/Wr;
+            
+            if(si <= Ri){
+                alert("PIZGIB OK");
+            }else{
+                alert("IZGIB MALO");
+                return false;
+            }
+            
+            var ts = (Q*S)/(Jb*b);
+//            alert('ts->'+ts+'\nRs->'+Rs);
+            if(ts <= Rs){
+                alert("PISKOLL OK");
+            }else{
+                alert("SKOLL MALO");
+                return false;
+            }
+            
+            var jsk = (5*Q*Math.pow(l,3))/(384*100000*Jb);
+            
+            if(jsk < 1/200){
+                alert("JESTKO");
+            }else{
+                alert("MJAGKO");
+            }
+            
+//            alert(q+'\n'+l+'\n'+M+'\n'+Msm+'\n'+Q+'\n'+jsk);
+
+            
+    });
+});
 
 
